0e0905f7af89fba59f2f662b0a44b0dd383df0ba
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-05-10 11:30:22 +0530

    "Refactor VideoCard and ChannelCard components, extract reusable components and update styling"

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/constants";
+import { CircleCheck } from "lucide-react";
 
-const ChannelCard = () => {
-  return (
-    <div>ChannelCard</div>
-  )
-}
+const ChannelCard = ({ channelDetail }) => {
+	return (
+		<div className="w-64 h-64 bg-[#000] flex flex-wrap items-center justify-center shadow-none rounded-none ">
+			<Link to={`/channel/${channelDetail?.id?.channelId}`}>
+				<div className="flex flex-col justify-center text-center text-[#fff]">
+					<img
+						className="mb-2 border-2 rounded-full w-44 h-44"
+						src={
+							channelDetail?.snippet?.thumbnails?.high?.url ||
+							demoProfilePicture
+						}
+						alt={channelDetail?.snippet?.title}
+					/>
+					<div className="flex items-center justify-center gap-2 font-medium">
+						{channelDetail?.snippet?.title}
+						<span>
+							<CircleCheck
+								fill="#999"
+								color="#000"
+								size={18}
+								strokeWidth={1}
+								absoluteStrokeWidth
+							/>
+						</span>
+					</div>
+					{channelDetail?.statistics?.subscriberCount && (
+						<div className="">
+							{parseInt(
+								channelDetail?.statistics?.subscriberCount
+							).toLocaleString()}{" "}
+							subscribers
+						</div>
+					)}
+				</div>
+			</Link>
+		</div>
+	);
+};
 
-export default ChannelCard
+export default ChannelCard;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,11 +4,11 @@ import { VideoCard, ChannelCard } from "../components";
 const Videos = ({ videos }) => {
 	return (
 		<div className="flex flex-wrap w-full h-screen overflow-y-hidden">
-			<div className="flex flex-wrap w-full h-full overflow-y-scroll scroll-smooth gap-x-4">
+			<div className="flex flex-wrap w-full h-full overflow-y-scroll scroll-smooth gap-x-6 gap-y-4">
 				{videos.map((item, index) => (
-					<div key={index} className="">
+					<div key={index}>
 						{item.id.videoId && <VideoCard video={item} />}
-						{/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
+						{item.id.channelId && <ChannelCard channelDetail={item} />}
 					</div>
 				))}
 			</div>
