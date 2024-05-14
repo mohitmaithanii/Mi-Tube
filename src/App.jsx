e2c6aff66b59bb52e14413aad64caaa5d3fd50cb
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-05-14 15:43:24 +0530

    "Added navigation and genre functionality to Carousel component; updated Carousel.jsx and style.scss"

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { fetchDataFromApi } from "./utils/api";
-import { getApiConfiguration } from "./store/homeSlice";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
 import {
 	Header,
@@ -18,23 +18,43 @@ import {
 const App = () => {
 	const dispatch = useDispatch();
 	const { url } = useSelector((state) => state.home);
+	console.log(url);
 
 	useEffect(() => {
 		fetchApiConfig();
+		genresCall();
 	}, []);
 
 	const fetchApiConfig = () => {
 		fetchDataFromApi("/configuration").then((res) => {
 			console.log(res);
 
-			const apiConfiguration = {
+			const url = {
 				backdrop: res.images.secure_base_url + "original",
 				poster: res.images.secure_base_url + "original",
 				profile: res.images.secure_base_url + "original",
 			};
 
-			dispatch(getApiConfiguration(apiConfiguration));
+			dispatch(getApiConfiguration(url));
 		});
+	};
+
+	const genresCall = async () => {
+		let promises = [];
+		let endPoints = ["tv", "movie"];
+		let allGenres = {};
+
+		endPoints.forEach((url) => {
+			promises.push(fetchDataFromApi(`/genre/${url}/list`));
+		});
+
+		const data = await Promise.all(promises);
+		console.log(data);
+		data.map(({ genres }) => {
+			return genres.map((item) => (allGenres[item.id] = item));
+		});
+
+		dispatch(getGenres(allGenres));
 	};
 
 	return (
