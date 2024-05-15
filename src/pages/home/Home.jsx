c40d4a1185c3ef28e527403aeb1483e6224f7c32
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-05-15 20:48:11 +0530

    home page complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,16 @@ import "./style.scss";
 
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
+import Popular from "./popular.jsx/Popular";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
 	return (
 		<div className="homePage">
 			<HeroBanner />
 			<Trending />
-			<div style={{ height: 1000 }} className=""></div>
+			<Popular />
+			<TopRated />
 		</div>
 	);
 };
